86ce568a1dfd7a14bb60c11ffd5d266878bb114c
Jerry Nixon <jerry.nixon@microsoft.com>
2025-03-31 17:48:43 -0600

    Fixed XML err

--- DbMaintenance/Procedures/usp_GetQueryPlan.sql
+++ DbMaintenance/Procedures/usp_GetQueryPlan.sql
@@ -1,11 +1,11 @@
-CREATE PROCEDURE maintenance.usp_GetQueryPlan
+ALTER PROCEDURE maintenance.usp_GetQueryPlan
     @QueryText NVARCHAR(MAX)
 AS
 BEGIN
     SET NOCOUNT ON;
 
     SELECT 
-        CAST(NULL AS XML) AS query_plan_xml,
+        CAST(NULL AS NVARCHAR(4000)) AS query_plan_xml,
         CAST(NULL AS NVARCHAR(4000)) AS query_text,
         CAST(NULL AS INT) AS estimated_cost
 END
